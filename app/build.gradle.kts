de747283f9fec1cd924811aa492fbcd8db8f4ca7
elena <mihalewa1995@gmail.com>
2025-08-04 23:07:29 +0500

    Fix: makefile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,18 @@
 plugins {
-    id("java")
-    id("application")
-    id("com.github.ben-manes.versions") version "0.51.0"  // Укажите версию явно
+    application
+    checkstyle
+    jacoco
 }
 
 group = "hexlet.code"
-version = "1.0-SNAPSHOT"
+version = "1.0.3-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+tasks.compileJava {
+    options.release.set(20)
 }
 
 application {
@@ -16,11 +20,31 @@ application {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(
+        "org.apache.commons:commons-lang3:3.12.0",
+        "com.fasterxml.jackson.core:jackson-databind:2.15.2",
+        "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.2",
+        "info.picocli:picocli:4.7.4"
+    )
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+
+    testImplementation(
+        "org.junit.jupiter:junit-jupiter-params:5.8.2",
+        "org.assertj:assertj-core:3.24.2"
+    )
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
+}
+
+tasks.run {
+    standardInput = System.`in`
 }
