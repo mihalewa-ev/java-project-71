cb8b92648c80b68dc6fcf98477e2bfcea8caf18b
elena <mihalewa1995@gmail.com>
2025-08-05 20:33:58 +0500

    Fix Formatter.java

--- app/src/main/java/hexlet/code/formatters/Formatter.java
+++ app/src/main/java/hexlet/code/formatters/Formatter.java
@@ -10,13 +10,13 @@ import static hexlet.code.formatters.PlainFormat.PLAIN_NAME;
 import static hexlet.code.formatters.JsonFormat.JSON_NAME;
 
 public class Formatter {
-    public static Map<String, Format> formats = Map.of(
+    private static final Map<String, Format> FORMATS = Map.of(
             STYLISH_NAME, new StylishFormat(),
             PLAIN_NAME, new PlainFormat(),
             JSON_NAME, new JsonFormat()
     );
 
-    /***
+    /**
      * Метод преобразует данные в выбранный/поддерживаемый формат.
      *
      * @param diffData данные для преобразования
@@ -25,6 +25,20 @@ public class Formatter {
      * @throws IOException если формат не поддерживается
      */
     public static String format(TreeMap<String, Node> diffData, String format) throws IOException {
-        return formats.get(format).generate(diffData);
+        Format formatter = getFormat(format);
+        if (formatter == null) {
+            throw new IOException("Unsupported format: " + format);
+        }
+        return formatter.generate(diffData);
+    }
+
+    /**
+     * Возвращает форматтер для указанного формата.
+     *
+     * @param format имя формата
+     * @return объект Format или null, если формат не найден
+     */
+    public static Format getFormat(String format) {
+        return FORMATS.get(format);
     }
 }
